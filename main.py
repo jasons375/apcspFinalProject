8c828061b49eecc8091cf7e35368eb7af000ebbd
jasons375 <jasons375@nycstudents.net>
2024-05-03 11:04:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("hi")
+import random
+import pygame
+import time
+
+
+# set up pygame modules
+pygame.init()
+pygame.font.init()
+pygame.display.set_caption("GOMOKU")
+screen = pygame.display.set_mode((720, 600))
+# fps
+clock = pygame.time.Clock()
+
+# COLORS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# TEXT FONTS
+font_title = pygame.font.SysFont("oldenglishtext", 72)
+
+# TEXT RENDER
+display_title_screen1 = font_title.render("GOMOKU.", True, BLACK)
+
+
+# variables
+
+# -------- Main Program Loop -----------
+while True:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+      
+    pygame.display.update()
+
+    clock.tick(60)
